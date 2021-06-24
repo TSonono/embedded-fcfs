12ba5f7e500803645ebbf2c154e70788bf0212cd
Tofik Sonono <tofik@sonono.me>
2021-06-24 12:19:12 +0200

    Skeleton code

---
+++ README.md
@@ -0,0 +1,12 @@
+# Light Weight "First Come, First Served" Scheduler in C
+
+This library is a generic "first come, first served" scheduler written in C, optimized for embedded. It follows the C99 standard, and only has one dependency (a fifo library).
+
+## Goals
+- Generic - The library should be useable in Soc:s from different vendors
+- No dynamic memory allocation
+- Low memory footprint
+- Minimal performance overhead
+
+## Contributing
+Submit a pull request. Please format your code using the .clang-format file, follow C99 and ensure that the code compiles and is tested.
---
+++ inc/fcfs.h
@@ -0,0 +1,25 @@
+#ifndef _fcfs_H_
+#define _fcfs_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @file fcfs.h
+ *
+ * Description
+ *
+ */
+
+/*======= Includes ==========================================================*/
+/*======= Public macro definitions ==========================================*/
+/*======= Type Definitions and declarations =================================*/
+/*======= Public variable declarations ======================================*/
+/*======= Public function declarations ======================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _fcfs_H_ */
